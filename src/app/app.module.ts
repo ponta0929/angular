ff09013c4f91537ccc229539fdd3945bdb8cb08d
ponta0929 <ponta.matumoto2@gmail.com>
2018-10-28 00:36:35 +0900

    イントロ：サービスのあたり

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './ponta/hero-detail/hero-detail.component';
+import { MessageComponent } from './ponta/message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROS } from "../heros.const";
+import { HeroService } from "../hero.service"
 
 @Component({
   selector: 'app-heroes',
@@ -9,18 +9,26 @@ import { HEROS } from "../heros.const";
 })
 export class HeroesComponent implements OnInit {
 
-  heros = HEROS;
+  heroes : Hero[];
   selectedHero : Hero;
 
 
-  constructor() {
+  constructor(
+    private heroService : HeroService
+  ) {
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero : Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
 
 }
